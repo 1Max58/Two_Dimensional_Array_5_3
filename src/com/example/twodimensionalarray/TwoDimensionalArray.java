7f5856f14ae8fafd90d4601babdd780ee3a5d88e
1Max58 <>
2023-11-08 18:21:18 +0200

    Creating an array implemented

--- src/com/example/twodimensionalarray/TwoDimensionalArray.java
+++ src/com/example/twodimensionalarray/TwoDimensionalArray.java
@@ -2,6 +2,17 @@ package com.example.twodimensionalarray;
 
 public class TwoDimensionalArray {
     public static void main(String[] args) {
-
+        System.out.println("This program creates two-dimensional array 10x10 and calculate the sum of the elements on the diagonal.");
+        int[][] nums = new int[10][10];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                if (j == i) {
+                    nums[i][j] = i;
+                }
+                else {
+                    nums[i][j] = 0;
+                }
+            }
+        }
     }
 }
